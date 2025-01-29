dff25a438a16d6ef63250d24ec0b1a187d2e0d6a
EmaCheat <ema2010346@gmail.com>
2025-01-29 23:36:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Moderando Siracusa", "Sta Giocando a Siracusa"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["Mettendo bambini in cantina", "Sta Giocando Mario Kart", "Sta mangiando sushi"];
+const statusTypes = [ 'dnd', 'idle', 'dnd',];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
